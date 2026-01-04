8d45509b0b449a45b3734545aabe0758f41fa4d6
ajayrabadiya24 <ajay.rabadiya@pmentire.com>
2026-01-04 12:40:08 +0530

    added package card design

--- templates/frontend/hotel-rooms.php
+++ templates/frontend/hotel-rooms.php
@@ -184,11 +184,46 @@ function format_room_description($room)
     </div>
 </div>
 
-<!-- First Package Design -->
+<?php $plugin_url = plugin_dir_url( dirname(__FILE__, 2) ); ?>
+
+<!-- Package Experience Design -->
 <div class="bys-packages">
     <div class="bys-packages-grid">
         <div class="bys-packages-card">
-            <div></div>
-        </div> 
+            <div class="bys-packages-card__top">
+                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url( $plugin_url . 'assets/images/package/1.png' ); ?>')"></div>
+                <div class="card__top-content">
+                    <img class="top-left__icon" src="<?php echo esc_url( $plugin_url . 'assets/images/icons/experience-icon.svg' ); ?>" alt="">
+                </div>
+                <div class="package-overlay">
+                    <span class="package-overlay__label">Package Experience</span>
+                    <h3 class="package-overlay__title">Easter in the Winelands <span>(3 Night Stay)</span></h3>
+                    <div class="package-overlay__divider"></div>
+                    <div class="package-overlay__location">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="18" viewBox="0 0 12 18">
+                            <path d="M6 0.75C3.1084 0.75 0.75 3.1084 0.75 6C0.75 8.63672 2.71289 10.8135 5.25 11.1797V18H6.75V11.1797C9.28711 10.8135 11.25 8.63672 11.25 6C11.25 3.1084 8.8916 0.75 6 0.75ZM6 2.25C8.08008 2.25 9.75 3.91992 9.75 6C9.75 8.08008 8.08008 9.75 6 9.75C3.91992 9.75 2.25 8.08008 2.25 6C2.25 3.91992 3.91992 2.25 6 2.25ZM6 3C4.35059 3 3 4.35059 3 6H4.5C4.5 5.16211 5.16211 4.5 6 4.5V3Z"></path>
+                        </svg>
+                        <div>
+                            <h6>Le Franschhoek Hotel & Spae</h6>
+                            <p>Franschhoek, ZA</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="bys-package-content">
+                <h4 class="package-content__title">INCLUDED</h4>
+                <div>
+                    <p class="package-content__description">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</p>
+                    <ul class="bys-package-features">
+                        <li>Stay 4 nights over Easter Weekend 2024 and get 30% off the 4th night</li>
+                        <li>An unforgettable 4-night stay in the beautiful winelands including breakfast</li>
+                        <li>Kids under 12 years stay free, including breakfast</li>
+                        <li>Easter Hunt included on Easter Sunday for the kids</li>
+                    </ul>
+                </div>
+                <div class="package-content__divider"></div>
+                <a href="#" class="bys-package-button">View Package</a>
+            </div>
+        </div>
     </div>
 </div>
