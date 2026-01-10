ef1cde7f06143abfbe1a8a908735ab5d5edeb104
DENISH-PERSONAL <142415013+DENISH-PERSONAL@users.noreply.github.com>
2026-01-10 19:25:01 +0530

    Wedding Packege Card

--- templates/frontend/hotel-rooms.php
+++ templates/frontend/hotel-rooms.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Hotel Rooms Display Template
  * Matches the bys-rooms design structure
@@ -103,10 +104,10 @@ function format_room_description($room)
             $first_image = !empty($room_images) ? $room_images[0] : 'https://dummyimage.com/1024x682/ccc/000';
             $has_images = $show_images && !empty($room_images);
             $room_price = 0; // Can be extended to get from pricing API
-            ?>
+        ?>
             <div class="bys-room-card">
                 <span class="bys-room-price"></span>
-    
+
                 <?php if ($has_images): ?>
                     <div class="bys-room-image">
                         <img src="<?php echo esc_url($first_image); ?>" alt="<?php echo esc_attr($room->room_type_name); ?>"
@@ -128,24 +129,24 @@ function format_room_description($room)
                         </div>
                     </div>
                 <?php endif; ?>
-    
+
                 <div class="bys-room-content">
                     <h3 class="bys-room-title"><?php echo esc_html($room->room_type_name); ?></h3>
-    
+
                     <?php if ($room->max_occupancy): ?>
                         <div class="bys-room-specs">
                             <span class="bys-room-specs-line"><?php echo esc_html($room->max_occupancy); ?>
                                 <?php echo $room->max_occupancy == 1 ? __('Guest', 'dhr-hotel-management') : __('Guests', 'dhr-hotel-management'); ?></span>
                         </div>
                     <?php endif; ?>
-    
+
                     <?php if ($show_amenities && !empty($room_amenities)): ?>
                         <ul class="bys-room-amenities">
                             <?php foreach ($room_amenities as $amenity):
                                 $amenity_name = isset($amenity['name']) ? $amenity['name'] : (is_string($amenity) ? $amenity : '');
                                 if (empty($amenity_name))
                                     continue;
-                                ?>
+                            ?>
                                 <li class="bys-room-amenity-item">
                                     <span class="bys-amenity-icon">
                                         <?php echo get_amenity_icon($amenity_name); ?>
@@ -155,13 +156,13 @@ function format_room_description($room)
                             <?php endforeach; ?>
                         </ul>
                     <?php endif; ?>
-    
+
                     <?php if ($show_description): ?>
                         <div class="bys-room-description">
                             <?php echo esc_html(format_room_description($room)); ?>
                         </div>
                     <?php endif; ?>
-    
+
                     <div class="bys-room-actions">
                         <a href="#" class="bys-book-now-link" data-room-code="<?php echo esc_attr($room->room_type_code); ?>"
                             data-hotel-code="<?php echo esc_attr($hotel_code); ?>" data-property-id=""
@@ -170,12 +171,18 @@ function format_room_description($room)
                             data-adults="<?php echo esc_attr($room->max_occupancy ?: 2); ?>" data-children="0" data-rooms="1">
                             <?php _e('Book Now', 'dhr-hotel-management'); ?>
                         </a>
-                        <!-- <a href="#" class="bys-book-now-button" data-room-code="<?php //echo esc_attr($room->room_type_code); ?>"
-                            data-hotel-code="<?php //echo esc_attr($hotel_code); ?>" data-property-id=""
-                            data-checkin="<?php //echo esc_attr(date('Y-m-d', strtotime('+1 day'))); ?>"
-                            data-checkout="<?php //echo esc_attr(date('Y-m-d', strtotime('+3 days'))); ?>"
-                            data-adults="<?php //echo esc_attr($room->max_occupancy ?: 2); ?>" data-children="0" data-rooms="1">
-                            <?php //_e('Book Now', 'dhr-hotel-management'); ?>
+                        <!-- <a href="#" class="bys-book-now-button" data-room-code="<?php //echo esc_attr($room->room_type_code); 
+                                                                                        ?>"
+                            data-hotel-code="<?php //echo esc_attr($hotel_code); 
+                                                ?>" data-property-id=""
+                            data-checkin="<?php //echo esc_attr(date('Y-m-d', strtotime('+1 day'))); 
+                                            ?>"
+                            data-checkout="<?php //echo esc_attr(date('Y-m-d', strtotime('+3 days'))); 
+                                            ?>"
+                            data-adults="<?php //echo esc_attr($room->max_occupancy ?: 2); 
+                                            ?>" data-children="0" data-rooms="1">
+                            <?php //_e('Book Now', 'dhr-hotel-management'); 
+                            ?>
                         </a> -->
                     </div>
                 </div>
@@ -184,18 +191,18 @@ function format_room_description($room)
     </div>
 </div>
 
-<?php $plugin_url = plugin_dir_url( dirname(__FILE__, 2) ); ?>
+<?php $plugin_url = plugin_dir_url(dirname(__FILE__, 2)); ?>
 
 <!-- Package Experience Design -->
 <div class="bys-packages">
-    
+
     <!-- First Package Design -->
     <div class="bys-packages-grid first-packages__design">
         <div class="bys-packages-card">
             <div class="bys-packages-card__top">
-                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url( $plugin_url . 'assets/images/package/1.png' ); ?>')"></div>
+                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url($plugin_url . 'assets/images/package/1.png'); ?>')"></div>
                 <div class="card__top-content">
-                    <img class="top-left__icon" src="<?php echo esc_url( $plugin_url . 'assets/images/icons/experience-icon.svg' ); ?>" alt="">
+                    <img class="top-left__icon" src="<?php echo esc_url($plugin_url . 'assets/images/icons/experience-icon.svg'); ?>" alt="">
                 </div>
                 <div class="package-overlay">
                     <span class="package-overlay__label">Package Experience</span>
@@ -231,9 +238,9 @@ function format_room_description($room)
         </div>
         <div class="bys-packages-card">
             <div class="bys-packages-card__top">
-                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url( $plugin_url . 'assets/images/package/1.png' ); ?>')"></div>
+                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url($plugin_url . 'assets/images/package/1.png'); ?>')"></div>
                 <div class="card__top-content">
-                    <img class="top-left__icon" src="<?php echo esc_url( $plugin_url . 'assets/images/icons/experience-icon.svg' ); ?>" alt="">
+                    <img class="top-left__icon" src="<?php echo esc_url($plugin_url . 'assets/images/icons/experience-icon.svg'); ?>" alt="">
                 </div>
                 <div class="package-overlay">
                     <span class="package-overlay__label">Package Experience</span>
@@ -269,9 +276,9 @@ function format_room_description($room)
         </div>
         <div class="bys-packages-card">
             <div class="bys-packages-card__top">
-                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url( $plugin_url . 'assets/images/package/1.png' ); ?>')"></div>
+                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url($plugin_url . 'assets/images/package/1.png'); ?>')"></div>
                 <div class="card__top-content">
-                    <img class="top-left__icon" src="<?php echo esc_url( $plugin_url . 'assets/images/icons/experience-icon.svg' ); ?>" alt="">
+                    <img class="top-left__icon" src="<?php echo esc_url($plugin_url . 'assets/images/icons/experience-icon.svg'); ?>" alt="">
                 </div>
                 <div class="package-overlay">
                     <span class="package-overlay__label">Package Experience</span>
@@ -307,14 +314,14 @@ function format_room_description($room)
         </div>
     </div>
     <br><br>
-    
+
     <!-- Second Package Design -->
     <div class="bys-packages-grid second-packages__design">
         <div class="bys-packages-card">
             <div class="bys-packages-card__top">
-                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url( $plugin_url . 'assets/images/package/1.png' ); ?>')"></div>
+                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url($plugin_url . 'assets/images/package/1.png'); ?>')"></div>
                 <div class="card__top-content">
-                    <img class="top-left__icon" src="<?php echo esc_url( $plugin_url . 'assets/images/icons/experience-icon.svg' ); ?>" alt="">
+                    <img class="top-left__icon" src="<?php echo esc_url($plugin_url . 'assets/images/icons/experience-icon.svg'); ?>" alt="">
                 </div>
                 <div class="package-overlay">
                     <span class="package-overlay__label">Package Experience</span>
@@ -350,9 +357,9 @@ function format_room_description($room)
         </div>
         <div class="bys-packages-card">
             <div class="bys-packages-card__top">
-                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url( $plugin_url . 'assets/images/package/1.png' ); ?>')"></div>
+                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url($plugin_url . 'assets/images/package/1.png'); ?>')"></div>
                 <div class="card__top-content">
-                    <img class="top-left__icon" src="<?php echo esc_url( $plugin_url . 'assets/images/icons/experience-icon.svg' ); ?>" alt="">
+                    <img class="top-left__icon" src="<?php echo esc_url($plugin_url . 'assets/images/icons/experience-icon.svg'); ?>" alt="">
                 </div>
                 <div class="package-overlay">
                     <span class="package-overlay__label">Package Experience</span>
@@ -388,9 +395,9 @@ function format_room_description($room)
         </div>
         <div class="bys-packages-card">
             <div class="bys-packages-card__top">
-                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url( $plugin_url . 'assets/images/package/1.png' ); ?>')"></div>
+                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url($plugin_url . 'assets/images/package/1.png'); ?>')"></div>
                 <div class="card__top-content">
-                    <img class="top-left__icon" src="<?php echo esc_url( $plugin_url . 'assets/images/icons/experience-icon.svg' ); ?>" alt="">
+                    <img class="top-left__icon" src="<?php echo esc_url($plugin_url . 'assets/images/icons/experience-icon.svg'); ?>" alt="">
                 </div>
                 <div class="package-overlay">
                     <span class="package-overlay__label">Package Experience</span>
@@ -431,9 +438,9 @@ function format_room_description($room)
     <div class="bys-packages-grid kids-packages__design">
         <div class="bys-packages-card">
             <div class="bys-packages-card__top">
-                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url( $plugin_url . 'assets/images/package/1.png' ); ?>')"></div>
+                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url($plugin_url . 'assets/images/package/1.png'); ?>')"></div>
                 <div class="card__top-content">
-                    <img class="top-left__icon" src="<?php echo esc_url( $plugin_url . 'assets/images/icons/experience-icon.svg' ); ?>" alt="">
+                    <img class="top-left__icon" src="<?php echo esc_url($plugin_url . 'assets/images/icons/experience-icon.svg'); ?>" alt="">
                 </div>
                 <div class="package-overlay">
                     <span class="package-overlay__label">Package Experience</span>
@@ -458,9 +465,9 @@ function format_room_description($room)
         </div>
         <div class="bys-packages-card">
             <div class="bys-packages-card__top">
-                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url( $plugin_url . 'assets/images/package/1.png' ); ?>')"></div>
+                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url($plugin_url . 'assets/images/package/1.png'); ?>')"></div>
                 <div class="card__top-content">
-                    <img class="top-left__icon" src="<?php echo esc_url( $plugin_url . 'assets/images/icons/experience-icon.svg' ); ?>" alt="">
+                    <img class="top-left__icon" src="<?php echo esc_url($plugin_url . 'assets/images/icons/experience-icon.svg'); ?>" alt="">
                 </div>
                 <div class="package-overlay">
                     <span class="package-overlay__label">Package Experience</span>
@@ -485,9 +492,9 @@ function format_room_description($room)
         </div>
         <div class="bys-packages-card">
             <div class="bys-packages-card__top">
-                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url( $plugin_url . 'assets/images/package/1.png' ); ?>')"></div>
+                <div class="bys-packages-card__frature-img" style="background-image: url('<?php echo esc_url($plugin_url . 'assets/images/package/1.png'); ?>')"></div>
                 <div class="card__top-content">
-                    <img class="top-left__icon" src="<?php echo esc_url( $plugin_url . 'assets/images/icons/experience-icon.svg' ); ?>" alt="">
+                    <img class="top-left__icon" src="<?php echo esc_url($plugin_url . 'assets/images/icons/experience-icon.svg'); ?>" alt="">
                 </div>
                 <div class="package-overlay">
                     <span class="package-overlay__label">Package Experience</span>
@@ -516,15 +523,32 @@ function format_room_description($room)
     <!-- Forth Package Design -->
     <div class="early-bird-packages-grid">
         <div class="bird-packages-card">
-            <div class="bird-packages-card__frature-img" style="background-image: url('<?php echo esc_url( $plugin_url . 'assets/images/package/2.png' ); ?>')"></div>
-            <div class="card__top-content">
-                <!-- <img class="top-left__icon" src="<?php echo esc_url( $plugin_url . 'assets/images/icons/experience-icon.svg' ); ?>" alt=""> -->
-            </div>
+            <div class="bird-packages-card__frature-img" style="background-image: url('<?php echo esc_url($plugin_url . 'assets/images/package/2.png'); ?>')"></div>
+
+            <div class="bird-packages-card__premium-img" style="background-image: url('<?php echo esc_url($plugin_url . 'assets/images/package/primium-img.png'); ?>')"></div>
             <div class="card__bottom-content">
-                <h3 class="package-overlay__main-title">Hello Spring Package</h3>
-                <p class="package-overlay__description">Celebrate your wedding with the start of summer.</p>
-                <span class="package-overlay__valid">Valid: 1 Sep - 15 Oct 2024</span>
-                <a href="#" class="bys-package-link">View Package</a>
+                <div class="card__top-badge">
+                    <p class="package-overlay__tag">
+                        Year End PACKAGE
+                    </p>
+                </div>
+                <div class="package-overlay__content">
+                    <div class="package-overlay__content__inner">
+                        <h3 class="package-overlay__main-title">Hello Spring Package</h3>
+                        <p class="package-overlay__description">Celebrate your wedding with the start of summer.</p>
+                        <span class="package-overlay__valid">Valid: 1 Sep - 15 Oct 2024</span>
+                    </div>
+                    <a href="#" class="package-overlay__link">
+                        View Package
+                        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <mask id="path-1-inside-1_5416_79089" fill="white">
+                                <path d="M5.67578 1.87305L5.04688 2.50195L9.54492 7L5.04688 11.498L5.67578 12.127L10.4883 7.31445L10.7891 7L10.4883 6.68555L5.67578 1.87305Z" />
+                            </mask>
+                            <path d="M5.67578 1.87305L5.04688 2.50195L9.54492 7L5.04688 11.498L5.67578 12.127L10.4883 7.31445L10.7891 7L10.4883 6.68555L5.67578 1.87305Z" fill="#EFF8FD" />
+                            <path d="M5.67578 1.87305L6.38289 1.16594L5.67578 0.458833L4.96867 1.16594L5.67578 1.87305ZM5.04688 2.50195L4.33977 1.79485L3.63266 2.50195L4.33977 3.20906L5.04688 2.50195ZM9.54492 7L10.252 7.70711L10.9591 7L10.252 6.29289L9.54492 7ZM5.04688 11.498L4.33977 10.7909L3.63266 11.498L4.33977 12.2052L5.04688 11.498ZM5.67578 12.127L4.96867 12.8341L5.67578 13.5412L6.38289 12.8341L5.67578 12.127ZM10.4883 7.31445L11.1954 8.02156L11.2032 8.0137L11.2109 8.00568L10.4883 7.31445ZM10.7891 7L11.5117 7.69122L12.1729 7L11.5117 6.30878L10.7891 7ZM10.4883 6.68555L11.2109 5.99432L11.2032 5.9863L11.1954 5.97844L10.4883 6.68555ZM4.96867 1.16594L4.33977 1.79485L5.75398 3.20906L6.38289 2.58015L4.96867 1.16594ZM4.33977 3.20906L8.83782 7.70711L10.252 6.29289L5.75398 1.79485L4.33977 3.20906ZM8.83782 6.29289L4.33977 10.7909L5.75398 12.2052L10.252 7.70711L8.83782 6.29289ZM4.33977 12.2052L4.96867 12.8341L6.38289 11.4198L5.75398 10.7909L4.33977 12.2052ZM6.38289 12.8341L11.1954 8.02156L9.78117 6.60735L4.96867 11.4198L6.38289 12.8341ZM11.2109 8.00568L11.5117 7.69122L10.0664 6.30878L9.76564 6.62323L11.2109 8.00568ZM11.5117 6.30878L11.2109 5.99432L9.76564 7.37677L10.0664 7.69122L11.5117 6.30878ZM11.1954 5.97844L6.38289 1.16594L4.96867 2.58015L9.78117 7.39265L11.1954 5.97844Z" fill="#D3AA74" mask="url(#path-1-inside-1_5416_79089)" />
+                        </svg>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
